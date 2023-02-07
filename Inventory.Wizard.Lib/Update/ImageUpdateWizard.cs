2b94dc73b614a32140222d1457dca08dcb0e8a3a
atari-monk <atari.monk1@gmail.com>
2023-02-07 16:22:57 +0100

    add old lib

---
+++ Inventory.Wizard.Lib/Update/ImageUpdateWizard.cs
@@ -0,0 +1,40 @@
+﻿using CLIReader;
+using CLIWizardHelper;
+using Inventory.Data;
+using Serilog;
+
+namespace Inventory.Wizard.Lib;
+
+public class ImageUpdateWizard 
+	: UpdateWizard<IInventoryUnitOfWork, Image>
+{
+    public ImageUpdateWizard(
+		IInventoryUnitOfWork unitOfWork
+		, IReader<string> requiredTextReader
+		, ILogger log) 
+			: base(unitOfWork, requiredTextReader, log)
+    {
+    }
+
+    protected override Image GetById(int id)
+    {
+        return UnitOfWork.Image.GetById(id) ?? new Image();
+	}
+
+    protected override void UpdateEntity(int nr, Image model)
+    {
+        switch (nr)
+        {
+			case 1:
+				var input = RequiredTextReader.Read(
+					new ReadConfig(6, nameof(Image.ItemId)));
+				ArgumentNullException.ThrowIfNull(input);
+				model.ItemId = int.Parse(input);
+				break;
+			case 2:
+				model.Path = RequiredTextReader.Read(
+					new ReadConfig(250, nameof(Image.Path)));
+				break;
+		}
+	}
+}
